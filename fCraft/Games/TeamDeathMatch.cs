e8b5d0ad50d326a985ca377b1862a2860dbac541
DingusBungus <themantimes8@aol.com>
2013-08-07 18:21:13 -0400

    Major TDM Update
    Added a scoreboard, new custom team features, team spawn points, starts the gun for the players, unhides the players and more!

--- fCraft/Games/TeamDeathMatch.cs
+++ fCraft/Games/TeamDeathMatch.cs
@@ -7,9 +7,9 @@ using System.Linq;
 using System.Text;
 using fCraft.Events;
 
-namespace fCraft.Games
+namespace fCraft
 {
-    public class TeamDeathMatch
+    class TeamDeathMatch
     {
         //Team Tags
         public const string redTeam = "&C-Red-";
@@ -32,6 +32,10 @@ namespace fCraft.Games
         public static int timeLimit = 300; 
         public static int scoreLimit = 50;
         public static int timeDelay = 20;
+        public static bool manualTeams = false;
+        public static int totalTime = timeLimit + timeDelay;
+        public static Position redSpawn = Position.Zero;
+        public static Position blueSpawn = Position.Zero;
         
         //TDM Game Bools
         public static bool isOn = false;
@@ -47,6 +51,10 @@ namespace fCraft.Games
                 instance = new TeamDeathMatch();
                 startTime = DateTime.Now;
                 task_ = new SchedulerTask(Interval, true).RunForever(TimeSpan.FromSeconds(1));
+                if (manualTeams)
+                {
+                    timeDelay = 30;
+                }
             }
             return instance;
         }
@@ -55,7 +63,7 @@ namespace fCraft.Games
         {
             world_.gameMode = GameMode.TeamDeathMatch; //set the game mode
             Scheduler.NewTask(t => world_.Players.Message("&WTEAM DEATHMATCH &fwill be starting in {0} seconds: &WGet ready!", timeDelay))
-                .RunRepeating(TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(10), 1);
+                    .RunRepeating(TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(10), 1);
         }
 
         public static void Stop(Player p) //for stopping the game early
@@ -65,18 +73,26 @@ namespace fCraft.Games
                 world_.Players.Message("{0}&S stopped the game of Team Deathmatch on world {1}",
                     p.ClassyName, world_.ClassyName);
             }
+            if (world_.Players.Count() > 1)
+            {
+                DisplayScoreBoard();
+            }
             RevertGame();          
             return;
         }
 
-        public static Random rand = new Random();
         public static void Interval(SchedulerTask task)
         {
             //check to stop Interval
-            if (world_.gameMode != GameMode.TeamDeathMatch || world_ == null)
+            if (world_ == null)
             {
-                world_ = null;
                 task.Stop();
+                return;
+            }
+            if (world_.gameMode != GameMode.TeamDeathMatch)
+            {
+                task.Stop();
+                world_ = null;
                 return;
             }
             if (!started)
@@ -90,19 +106,25 @@ namespace fCraft.Games
                 {
                     foreach (Player p in world_.Players)
                     {
-                        int x = rand.Next(2, world_.Map.Width);
-                        int y = rand.Next(2, world_.Map.Length);
-                        int z1 = 0;
-                        for (int z = world_.Map.Height - 1; z > 0; z--)
+                        if (!manualTeams)
                         {
-                            if (world_.Map.GetBlock(x, y, z) != Block.Air)
-                            {
-                                z1 = z + 3;
-                                break;
-                            }
+                            assignTeams(p); //assigns teams (who knew?)
                         }
-                        p.TeleportTo(new Position(x, y, z1 + 2).ToVector3I().ToPlayerCoords()); //teleport players to a random position
-                        assignTeams(p); //assigns teams (who knew?)
+                        if (p.Info.isOnRedTeam) { p.TeleportTo(TeamDeathMatch.redSpawn); } //teleport players to the team spawn
+                        if (p.Info.isOnBlueTeam) { p.TeleportTo(TeamDeathMatch.blueSpawn); }
+
+                        if (!p.GunMode)
+                        {
+                            p.GunMode = true; //turns on gunMode automatically if not already on
+                            GunGlassTimer timer = new GunGlassTimer(p);
+                            timer.Start();
+                        }
+
+                        if (p.Info.IsHidden) //unhides players automatically if hidden (cannot shoot guns while hidden)
+                        {
+                            p.Info.IsHidden = false;
+                            Player.RaisePlayerHideChangedEvent(p);
+                        }
                     }
                     started = true;   //the game has officially started
                     isOn = true;
@@ -112,41 +134,27 @@ namespace fCraft.Games
                     }
                     lastChecked = DateTime.Now;     //used for intervals
                     return;
-                }
+                }   
             }
 
             //check if one of the teams have won
-            if (redScore >= scoreLimit)
+            if (redScore >= scoreLimit || blueScore >= scoreLimit)
             {
-                world_.Players.Message("&fThe &cRed Team&f has won {1} to {0}!", blueScore, redScore);
-                Stop(null);
-                return;
-            }
-            if (blueScore >= scoreLimit)
-            {
-                world_.Players.Message("&fThe &1Blue Team&f has won {1} to {0}!", redScore, blueScore);
                 Stop(null);
                 return;
             }
             if (blueScore == scoreLimit && redScore == scoreLimit) //if they somehow manage to tie which I am pretty sure is impossible
             {
-                world_.Players.Message("&fThe teams have tied!");
+                world_.Players.Message("The teams tied at {0}!", redScore);
                 Stop(null);
                 return;
             }
 
-            //check if time is up (delay time + start time)
-            if (started && startTime != null && (DateTime.Now - startTime).TotalSeconds >= (timeDelay + timeLimit))
+            //check if time is up
+            if (started && startTime != null && (DateTime.Now - startTime).TotalSeconds >= (totalTime))
             {
-                if (redScore > blueScore)
+                if (redScore != blueScore)
                 {
-                    world_.Players.Message("&fThe &cRed Team&f has won {0} to {1}.", redScore, blueScore);
-                    Stop(null);
-                    return;
-                }
-                if (redScore < blueScore)
-                {
-                    world_.Players.Message("&fThe &1Blue Team&f has won {0} to {1}.", blueScore, redScore);
                     Stop(null);
                     return;
                 }
@@ -196,7 +204,7 @@ namespace fCraft.Games
             }
             timeLeft = Convert.ToInt16(((timeDelay + timeLimit) - (DateTime.Now - startTime).TotalSeconds));
             //Keep the players updated about the score
-            if (lastChecked != null && (DateTime.Now - lastChecked).TotalSeconds > 29.9 && timeLeft <= timeLimit)
+            if (lastChecked != null && (DateTime.Now - lastChecked).TotalSeconds > 29.8 && timeLeft <= timeLimit)
             {               
                 if (redScore > blueScore)
                 {
@@ -223,89 +231,74 @@ namespace fCraft.Games
 
         static public void assignTeams(Player p)    //Assigns teams to all players in the world
         {
-            if (redTeamCount == 0) //if there are no players assigned to any team yet
-            {
-                p.Message("Let the games Begin! Type &H/Gun"); 
-                p.Message("You are on the &cRed Team"); 
-                p.iName = Color.Red + p.Name;
-                p.Info.tdmOldName = p.Info.DisplayedName;
-                p.Info.DisplayedName = "&f(" + redTeam + "&f) " + Color.Red + p.Name;   
-                p.Info.isOnRedTeam = true;      
-                p.Info.isOnBlueTeam = false;
-                p.Info.isPlayingTD = true;      
-                p.entityChanged = true;         
-                p.Info.gameKills = 0;
-                p.Info.gameDeaths = 0;
-                redTeamCount++;                 
-                return;
-            }
-            if (blueTeamCount < redTeamCount) //if the red team has more players and the red team has already been assigned at least one player
-            {
-                p.Message("Let the games Begin! Type &H/Gun");
-                p.Message("You are on the &1Blue Team");
-                p.iName = Color.Navy + p.Name;
-                p.Info.tdmOldName = p.Info.DisplayedName;
-                p.Info.DisplayedName = "&f(" + blueTeam + "&f) " + Color.Navy + p.Name;
-                p.Info.isOnBlueTeam = true;
-                p.Info.isOnRedTeam = false;
-                p.Info.isPlayingTD = true;
-                p.entityChanged = true;
-                p.Info.gameKills = 0;
-                p.Info.gameDeaths = 0;
-                blueTeamCount++;
-                return;
-            }
-            if (blueTeamCount > redTeamCount)   //if the blue team has more players than the red team (shouldn't be necessary but in case of bugs)
-            {
-                p.Message("Let the games Begin! Type &H/Gun");
-                p.Message("You are on the &cRed Team"); //notifies the player of their team
-                p.iName = Color.Red + p.Name;   //the name above their head will be colored blue or red depending on team
-                p.Info.tdmOldName = p.Info.DisplayedName;
-                p.Info.DisplayedName = "&f(" + redTeam + "&f) " + Color.Red + p.Name;   //changes their displayed name to include a little tag and their real mc username colored their team color
-                p.Info.isOnRedTeam = true;      //assigns to red team
-                p.Info.isOnBlueTeam = false;
-                p.Info.isPlayingTD = true;      //they are now officially "playing TD"
-                p.entityChanged = true;         //reloads skin and name above head to become the new ones
-                p.Info.gameKills = 0;           //resets their kills from last game
-                p.Info.gameDeaths = 0;          //resets their deaths from last game
-                redTeamCount++;                 //add 1 to the red or blue team player count
-                return;
-            }
-            if (blueTeamCount == redTeamCount)  //if the teams have the same number of players, by default the next player will be assigned to the red team
-            {
-                p.Message("Let the games Begin! Type &H/Gun");
-                p.Message("You are on the &1Blue Team");
-                p.iName = Color.Navy + p.Name;
-                p.Info.tdmOldName = p.Info.DisplayedName;
-                p.Info.DisplayedName = "&f(" + blueTeam + "&f) " + Color.Navy + p.Name;
-                p.Info.isOnBlueTeam = true;
-                p.Info.isOnRedTeam = false;
-                p.Info.isPlayingTD = true;
-                p.entityChanged = true;
-                p.Info.gameKills = 0;
-                p.Info.gameDeaths = 0;
-                blueTeamCount++;
-                return;
-            }
+            //if there are no players assigned to any team yet
+            if (redTeamCount == 0) { AssignRed(p); return; }
+            //if the red team has more players and the red team has already been assigned at least one player
+            if (blueTeamCount < redTeamCount) { AssignBlue(p); return; }
+            //if the teams have the same number of players, by default the next player will be assigned to the red team
+            if (blueTeamCount == redTeamCount) { AssignRed(p); return; }
         }
 
         public static void RevertGame() //Reset game bools/stats and stop timers
         {
+            task_.Stop();
             world_.gameMode = GameMode.NULL;
+            isOn = false;
+            instance = null;
+            started = false;
             if (world_.gunPhysics)
             {
                 world_.DisableGunPhysics(Player.Console, true);
             }
-            task_.Stop();
-            isOn = false;
-            instance = null;
-            started = false;
             world_ = null;
             redScore = 0;
             blueScore = 0;
             redTeamCount = 0;
             blueTeamCount = 0;
             RevertNames();
+        }
+
+        public static void DisplayScoreBoard()
+        {
+            List<PlayerInfo> TDPlayersRed = new List<PlayerInfo>(PlayerDB.PlayerInfoList.Where(r => r.isPlayingTD).Where(r => r.isOnRedTeam).ToArray().OrderBy(r => r.totalKillsTDM).Reverse());
+            List<PlayerInfo> TDPlayersBlue = new List<PlayerInfo>(PlayerDB.PlayerInfoList.Where(r => r.isPlayingTD).Where(r => r.isOnBlueTeam).ToArray().OrderBy(r => r.totalKillsTDM).Reverse());
+            if (redScore < blueScore)
+            {
+                world_.Players.Message("&1Blue Team &f{0}&1:", TeamDeathMatch.blueScore);
+                for (int i = 0; i < TDPlayersBlue.Count(); i++)
+                {
+                    string sbNameBlue = TDPlayersBlue[i].Name;
+                    world_.Players.Message("&1{0} &f| &c{1} &fKills - &c{2} &fDeaths",
+                        sbNameBlue, TDPlayersBlue[i].gameKills, TDPlayersBlue[i].gameDeaths);
+                }
+                world_.Players.Message("&f--------------------------------------------");
+                world_.Players.Message("&CRed Team &f{0}&1:", TeamDeathMatch.redScore);
+                for (int i = 0; i < TDPlayersRed.Count(); i++)
+                {
+                    string sbNameRed = TDPlayersRed[i].Name;
+                    world_.Players.Message("&C{0} &f| &c{1} &fKills - &c{2} &fDeaths",
+                        sbNameRed, TDPlayersRed[i].gameKills, TDPlayersRed[i].gameDeaths);
+                }
+            }
+            if (redScore >= blueScore)
+            {
+                world_.Players.Message("&CRed Team &f{0}&1:", TeamDeathMatch.redScore);
+                for (int i = 0; i < TDPlayersRed.Count(); i++)
+                {
+                    string sbNameRed = TDPlayersRed[i].Name;
+                    world_.Players.Message("&C{0} &f| &c{1} &fKills - &c{2} &fDeaths",
+                        sbNameRed, TDPlayersRed[i].gameKills, TDPlayersRed[i].gameDeaths);
+                }
+                world_.Players.Message("&f--------------------------------------------");
+                world_.Players.Message("&1Blue Team &f{0}&1:", TeamDeathMatch.blueScore);
+                for (int i = 0; i < TDPlayersBlue.Count(); i++)
+                {
+                    string sbNameBlue = TDPlayersBlue[i].Name;
+                    world_.Players.Message("&1{0} &f| &c{1} &fKills - &c{2} &fDeaths",
+                        sbNameBlue, TDPlayersBlue[i].gameKills, TDPlayersBlue[i].gameDeaths);
+                }              
+            }
+            return;
         }
         
         public static void RevertNames()    //reverts names for online players. offline players get reverted upon leaving the game
@@ -320,17 +313,96 @@ namespace fCraft.Games
                 if (p != null)
                 {
                     p.iName = null;
-                    pI.DisplayedName = pI.tdmOldName;
+                    pI.DisplayedName = pI.oldname;
                     pI.isOnRedTeam = false;
                     pI.isOnBlueTeam = false;
                     pI.isPlayingTD = false;
                     p.entityChanged = true;
+                    
+                    //undo gunmode (taken from GunHandler.cs)
+                    p.GunMode = false;
+                    try
+                    {
+                        foreach (Vector3I block in p.GunCache.Values)
+                        {
+                            p.Send(PacketWriter.MakeSetBlock(block.X, block.Y, block.Z, p.WorldMap.GetBlock(block)));
+                            Vector3I removed;
+                            p.GunCache.TryRemove(block.ToString(), out removed);
+                        }
+                        if (p.bluePortal.Count > 0)
+                        {
+                            int j = 0;
+                            foreach (Vector3I block in p.bluePortal)
+                            {
+                                if (p.WorldMap != null && p.World.IsLoaded)
+                                {
+                                    p.WorldMap.QueueUpdate(new BlockUpdate(null, block, p.blueOld[j]));
+                                    j++;
+                                }
+                            }
+                            p.blueOld.Clear();
+                            p.bluePortal.Clear();
+                        }
+                        if (p.orangePortal.Count > 0)
+                        {
+                            int j = 0;
+                            foreach (Vector3I block in p.orangePortal)
+                            {
+                                if (p.WorldMap != null && p.World.IsLoaded)
+                                {
+                                    p.WorldMap.QueueUpdate(new BlockUpdate(null, block, p.orangeOld[j]));
+                                    j++;
+                                }
+                            }
+                            p.orangeOld.Clear();
+                            p.orangePortal.Clear();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.Log(LogType.SeriousError, "" + ex);
+                    }
                     if (p.IsOnline)
                     {
                         p.Message("Your status has been reverted.");
                     }
                 }
             }
+        }
+
+        public static void AssignRed(Player p)
+        {
+            string sbName = p.Name;
+            p.Message("Let the games Begin!");
+            p.Message("You are on the &cRed Team");
+            p.iName = Color.Red + sbName;
+            p.Info.oldname = p.Info.DisplayedName;
+            p.Info.DisplayedName = "&f(" + redTeam + "&f) " + Color.Red + sbName;
+            p.Info.isOnRedTeam = true;
+            p.Info.isOnBlueTeam = false;
+            p.Info.isPlayingTD = true;
+            p.entityChanged = true;
+            p.Info.gameKills = 0;
+            p.Info.gameDeaths = 0;
+            redTeamCount++;
+            return;
+        }
+        public static void AssignBlue(Player p)
+        {
+            string sbName = p.Name;
+            p.Message("Let the games Begin!");
+            p.Message("You are on the &1Blue Team");
+            p.iName = Color.Navy + sbName;
+            p.Info.oldname = p.Info.DisplayedName;
+            p.Info.DisplayedName = "&f(" + blueTeam + "&f) " + Color.Navy + sbName;
+            p.Info.isOnBlueTeam = true;
+            p.Info.isOnRedTeam = false;
+            p.Info.isPlayingTD = true;
+            p.entityChanged = true;
+            p.Info.gameKills = 0;
+            p.Info.gameDeaths = 0;
+            blueTeamCount++;
+            return;
         }
     }
 }
