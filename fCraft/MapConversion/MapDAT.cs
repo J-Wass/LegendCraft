d9cf8c228a32129f522a2ab2a5405605d8a2aded
LeChosenOne <jonahw@optonline.net>
2013-11-11 19:32:13 -0500

    merge,  i think?

--- fCraft/MapConversion/MapDAT.cs
+++ fCraft/MapConversion/MapDAT.cs
@@ -6,28 +6,53 @@ using System.IO.Compression;
 using System.Net;
 using JetBrains.Annotations;
 
-namespace fCraft.MapConversion {
-    public sealed class MapDat : IMapConverter {
+namespace fCraft.MapConversion
+{
+    public sealed class MapDat : IMapConverter
+    {
 
         static readonly byte[] Mapping = new byte[256];
 
-        static MapDat() {
-            Mapping[50] = (byte)Block.Air;      // torch
-            Mapping[51] = (byte)Block.Lava;     // fire
-            Mapping[52] = (byte)Block.Glass;    // spawner
-            Mapping[53] = (byte)Block.Stair;    // wood stairs
-            Mapping[54] = (byte)Block.Wood;     // chest
-            Mapping[55] = (byte)Block.Air;      // redstone wire
-            Mapping[56] = (byte)Block.IronOre;  // diamond ore
-            Mapping[57] = (byte)Block.Aqua;     // diamond block
-            Mapping[58] = (byte)Block.Log;      // workbench
-            Mapping[59] = (byte)Block.Leaves;   // crops
-            Mapping[60] = (byte)Block.Dirt;     // soil
-            Mapping[61] = (byte)Block.Stone;    // furnace
-            Mapping[62] = (byte)Block.Stone;    // burning furnance
-            Mapping[63] = (byte)Block.Air;      // sign post
-            Mapping[64] = (byte)Block.Air;      // wooden door
-            Mapping[65] = (byte)Block.Air;      // ladder
+        static MapDat()
+        {
+            if (!Heartbeat.ClassiCube()) //if using mc.net
+            {
+                Mapping[50] = (byte)Block.Air;      // torch
+                Mapping[51] = (byte)Block.Lava;     // fire
+                Mapping[52] = (byte)Block.Glass;    // spawner
+                Mapping[53] = (byte)Block.Stair;    // wood stairs
+                Mapping[54] = (byte)Block.Wood;     // chest
+                Mapping[55] = (byte)Block.Air;      // redstone wire
+                Mapping[56] = (byte)Block.IronOre;  // diamond ore
+                Mapping[57] = (byte)Block.Aqua;     // diamond block
+                Mapping[58] = (byte)Block.Log;      // workbench
+                Mapping[59] = (byte)Block.Leaves;   // crops
+                Mapping[60] = (byte)Block.Dirt;     // soil
+                Mapping[61] = (byte)Block.Stone;    // furnace
+                Mapping[62] = (byte)Block.Stone;    // burning furnance
+                Mapping[63] = (byte)Block.Air;      // sign post
+                Mapping[64] = (byte)Block.Air;      // wooden door
+                Mapping[65] = (byte)Block.Air;      // ladder
+            }
+            else     //classicube
+            {
+                Mapping[50] = (byte)Block.CobbleSlab;   // torch
+                Mapping[51] = (byte)Block.Rope;         // fire
+                Mapping[52] = (byte)Block.Sandstone;    // spawner
+                Mapping[53] = (byte)Block.Snow;         // wood stairs
+                Mapping[54] = (byte)Block.Fire;         // chest
+                Mapping[55] = (byte)Block.LightPink;    // redstone wire
+                Mapping[56] = (byte)Block.DarkGreen;    // diamond ore
+                Mapping[57] = (byte)Block.Brown;        // diamond block
+                Mapping[58] = (byte)Block.DarkBlue;     // workbench
+                Mapping[59] = (byte)Block.Turquoise;    // crops
+                Mapping[60] = (byte)Block.Ice;          // soil
+                Mapping[61] = (byte)Block.Tile;         // furnace
+                Mapping[62] = (byte)Block.Magma;        // burning furnance
+                Mapping[63] = (byte)Block.Pillar;       // sign post
+                Mapping[64] = (byte)Block.Crate;        // wooden door
+                Mapping[65] = (byte)Block.StoneBrick;   // ladder
+            }
             Mapping[66] = (byte)Block.Air;      // rails
             Mapping[67] = (byte)Block.Stair;    // cobblestone stairs
             Mapping[68] = (byte)Block.Air;      // wall sign
@@ -58,99 +83,120 @@ namespace fCraft.MapConversion {
         }
 
 
-        public string ServerName {
+        public string ServerName
+        {
             get { return "Creative/Vanilla"; }
         }
 
 
-        public MapStorageType StorageType {
+        public MapStorageType StorageType
+        {
             get { return MapStorageType.SingleFile; }
         }
 
 
-        public MapFormat Format {
+        public MapFormat Format
+        {
             get { return MapFormat.Creative; }
         }
 
 
-        public bool ClaimsName( [NotNull] string fileName ) {
-            if( fileName == null ) throw new ArgumentNullException( "fileName" );
-            return fileName.EndsWith( ".dat", StringComparison.OrdinalIgnoreCase ) ||
-                   fileName.EndsWith( ".mine", StringComparison.OrdinalIgnoreCase );
+        public bool ClaimsName([NotNull] string fileName)
+        {
+            if (fileName == null) throw new ArgumentNullException("fileName");
+            return fileName.EndsWith(".dat", StringComparison.OrdinalIgnoreCase) ||
+                   fileName.EndsWith(".mine", StringComparison.OrdinalIgnoreCase);
         }
 
 
-        public bool Claims( [NotNull] string fileName ) {
-            if( fileName == null ) throw new ArgumentNullException( "fileName" );
-            try {
-                using( FileStream mapStream = File.OpenRead( fileName ) ) {
+        public bool Claims([NotNull] string fileName)
+        {
+            if (fileName == null) throw new ArgumentNullException("fileName");
+            try
+            {
+                using (FileStream mapStream = File.OpenRead(fileName))
+                {
                     byte[] temp = new byte[8];
-                    mapStream.Seek( -4, SeekOrigin.End );
-                    mapStream.Read( temp, 0, 4 );
-                    mapStream.Seek( 0, SeekOrigin.Begin );
-                    int length = BitConverter.ToInt32( temp, 0 );
+                    mapStream.Seek(-4, SeekOrigin.End);
+                    mapStream.Read(temp, 0, 4);
+                    mapStream.Seek(0, SeekOrigin.Begin);
+                    int length = BitConverter.ToInt32(temp, 0);
                     byte[] data = new byte[length];
-                    using( GZipStream reader = new GZipStream( mapStream, CompressionMode.Decompress, true ) ) {
-                        reader.Read( data, 0, length );
+                    using (GZipStream reader = new GZipStream(mapStream, CompressionMode.Decompress, true))
+                    {
+                        reader.Read(data, 0, length);
                     }
 
-                    for( int i = 0; i < length - 1; i++ ) {
-                        if( data[i] == 0xAC && data[i + 1] == 0xED ) {
+                    for (int i = 0; i < length - 1; i++)
+                    {
+                        if (data[i] == 0xAC && data[i + 1] == 0xED)
+                        {
                             return true;
                         }
                     }
                     return false;
                 }
-            } catch( Exception ) {
+            }
+            catch (Exception)
+            {
                 return false;
             }
         }
 
 
-        public Map LoadHeader( [NotNull] string fileName ) {
-            if( fileName == null ) throw new ArgumentNullException( "fileName" );
-            Map map = Load( fileName );
+        public Map LoadHeader([NotNull] string fileName)
+        {
+            if (fileName == null) throw new ArgumentNullException("fileName");
+            Map map = Load(fileName);
             map.Blocks = null;
             return map;
         }
 
 
-        public static byte MapBlock( byte block ) {
+        public static byte MapBlock(byte block)
+        {
             return Mapping[block];
         }
 
-        public static Block MapBlock( Block block ) {
+        public static Block MapBlock(Block block)
+        {
             return (Block)Mapping[(byte)block];
         }
 
-        public Map Load( [NotNull] string fileName ) {
-            if( fileName == null ) throw new ArgumentNullException( "fileName" );
-            using( FileStream mapStream = File.OpenRead( fileName ) ) {
+        public Map Load([NotNull] string fileName)
+        {
+            if (fileName == null) throw new ArgumentNullException("fileName");
+            using (FileStream mapStream = File.OpenRead(fileName))
+            {
                 byte[] temp = new byte[8];
                 Map map = null;
 
-                mapStream.Seek( -4, SeekOrigin.End );
-                mapStream.Read( temp, 0, 4 );
-                mapStream.Seek( 0, SeekOrigin.Begin );
-                int uncompressedLength = BitConverter.ToInt32( temp, 0 );
+                mapStream.Seek(-4, SeekOrigin.End);
+                mapStream.Read(temp, 0, 4);
+                mapStream.Seek(0, SeekOrigin.Begin);
+                int uncompressedLength = BitConverter.ToInt32(temp, 0);
                 byte[] data = new byte[uncompressedLength];
-                using( GZipStream reader = new GZipStream( mapStream, CompressionMode.Decompress, true ) ) {
-                    reader.Read( data, 0, uncompressedLength );
+                using (GZipStream reader = new GZipStream(mapStream, CompressionMode.Decompress, true))
+                {
+                    reader.Read(data, 0, uncompressedLength);
                 }
 
-                for( int i = 0; i < uncompressedLength - 1; i++ ) {
-                    if( data[i] != 0xAC || data[i + 1] != 0xED ) continue;
+                for (int i = 0; i < uncompressedLength - 1; i++)
+                {
+                    if (data[i] != 0xAC || data[i + 1] != 0xED) continue;
 
                     // bypassing the header crap
                     int pointer = i + 6;
-                    Array.Copy( data, pointer, temp, 0, 2 );
-                    pointer += IPAddress.HostToNetworkOrder( BitConverter.ToInt16( temp, 0 ) );
+                    Array.Copy(data, pointer, temp, 0, 2);
+                    pointer += IPAddress.HostToNetworkOrder(BitConverter.ToInt16(temp, 0));
                     pointer += 13;
 
                     int headerEnd;
                     // find the end of serialization listing
-                    for( headerEnd = pointer; headerEnd < data.Length - 1; headerEnd++ ) {
-                        if( data[headerEnd] == 0x78 && data[headerEnd + 1] == 0x70 ) {
+                    for (headerEnd = pointer; headerEnd < data.Length - 1; headerEnd++)
+                    {
+                        if (data[headerEnd] == 0x78 && data[headerEnd + 1] == 0x70)
+                        {
                             headerEnd += 2;
                             break;
                         }
@@ -160,8 +206,10 @@ namespace fCraft.MapConversion {
                     int offset = 0;
                     int width = 0, length = 0, height = 0;
                     Position spawn = new Position();
-                    while( pointer < headerEnd ) {
-                        switch( (char)data[pointer] ) {
+                    while (pointer < headerEnd)
+                    {
+                        switch ((char)data[pointer])
+                        {
                             case 'Z':
                                 offset++;
                                 break;
@@ -175,53 +223,70 @@ namespace fCraft.MapConversion {
                         }
 
                         pointer += 1;
-                        Array.Copy( data, pointer, temp, 0, 2 );
-                        short skip = IPAddress.HostToNetworkOrder( BitConverter.ToInt16( temp, 0 ) );
+                        Array.Copy(data, pointer, temp, 0, 2);
+                        short skip = IPAddress.HostToNetworkOrder(BitConverter.ToInt16(temp, 0));
                         pointer += 2;
 
                         // look for relevant variables
-                        Array.Copy( data, headerEnd + offset - 4, temp, 0, 4 );
-                        if( MemCmp( data, pointer, "width" ) ) {
-                            width = (ushort)IPAddress.HostToNetworkOrder( BitConverter.ToInt32( temp, 0 ) );
-                        } else if( MemCmp( data, pointer, "depth" ) ) {
-                            height = (ushort)IPAddress.HostToNetworkOrder( BitConverter.ToInt32( temp, 0 ) );
-                        } else if( MemCmp( data, pointer, "height" ) ) {
-                            length = (ushort)IPAddress.HostToNetworkOrder( BitConverter.ToInt32( temp, 0 ) );
-                        } else if( MemCmp( data, pointer, "xSpawn" ) ) {
-                            spawn.X = (short)( IPAddress.HostToNetworkOrder( BitConverter.ToInt32( temp, 0 ) ) * 32 + 16 );
-                        } else if( MemCmp( data, pointer, "ySpawn" ) ) {
-                            spawn.Z = (short)( IPAddress.HostToNetworkOrder( BitConverter.ToInt32( temp, 0 ) ) * 32 + 16 );
-                        } else if( MemCmp( data, pointer, "zSpawn" ) ) {
-                            spawn.Y = (short)( IPAddress.HostToNetworkOrder( BitConverter.ToInt32( temp, 0 ) ) * 32 + 16 );
+                        Array.Copy(data, headerEnd + offset - 4, temp, 0, 4);
+                        if (MemCmp(data, pointer, "width"))
+                        {
+                            width = (ushort)IPAddress.HostToNetworkOrder(BitConverter.ToInt32(temp, 0));
+                        }
+                        else if (MemCmp(data, pointer, "depth"))
+                        {
+                            height = (ushort)IPAddress.HostToNetworkOrder(BitConverter.ToInt32(temp, 0));
+                        }
+                        else if (MemCmp(data, pointer, "height"))
+                        {
+                            length = (ushort)IPAddress.HostToNetworkOrder(BitConverter.ToInt32(temp, 0));
+                        }
+                        else if (MemCmp(data, pointer, "xSpawn"))
+                        {
+                            spawn.X = (short)(IPAddress.HostToNetworkOrder(BitConverter.ToInt32(temp, 0)) * 32 + 16);
+                        }
+                        else if (MemCmp(data, pointer, "ySpawn"))
+                        {
+                            spawn.Z = (short)(IPAddress.HostToNetworkOrder(BitConverter.ToInt32(temp, 0)) * 32 + 16);
+                        }
+                        else if (MemCmp(data, pointer, "zSpawn"))
+                        {
+                            spawn.Y = (short)(IPAddress.HostToNetworkOrder(BitConverter.ToInt32(temp, 0)) * 32 + 16);
                         }
 
                         pointer += skip;
                     }
 
-                    map = new Map( null, width, length, height, false ) { Spawn = spawn };
+                    map = new Map(null, width, length, height, false) { Spawn = spawn };
 
-                    if( !map.ValidateHeader() ) {
-                        throw new MapFormatException( "One or more of the map dimensions are invalid." );
+                    if (!map.ValidateHeader())
+                    {
+                        throw new MapFormatException("One or more of the map dimensions are invalid.");
                     }
 
                     // find the start of the block array
                     bool foundBlockArray = false;
-                    offset = Array.IndexOf<byte>( data, 0x00, headerEnd );
-                    while( offset != -1 && offset < data.Length - 2 ) {
-                        if( data[offset] == 0x00 && data[offset + 1] == 0x78 && data[offset + 2] == 0x70 ) {
+                    offset = Array.IndexOf<byte>(data, 0x00, headerEnd);
+                    while (offset != -1 && offset < data.Length - 2)
+                    {
+                        if (data[offset] == 0x00 && data[offset + 1] == 0x78 && data[offset + 2] == 0x70)
+                        {
                             foundBlockArray = true;
                             pointer = offset + 7;
                         }
-                        offset = Array.IndexOf<byte>( data, 0x00, offset + 1 );
+                        offset = Array.IndexOf<byte>(data, 0x00, offset + 1);
                     }
 
                     // copy the block array... or fail
-                    if( foundBlockArray ) {
+                    if (foundBlockArray)
+                    {
                         map.Blocks = new byte[map.Volume];
-                        Array.Copy( data, pointer, map.Blocks, 0, map.Blocks.Length );
-                        map.ConvertBlockTypes( Mapping );
-                    } else {
-                        throw new MapFormatException( "Could not locate block array." );
+                        Array.Copy(data, pointer, map.Blocks, 0, map.Blocks.Length);
+                        map.ConvertBlockTypes(Mapping);
+                    }
+                    else
+                    {
+                        throw new MapFormatException("Could not locate block array.");
                     }
                     break;
                 }
@@ -230,19 +295,22 @@ namespace fCraft.MapConversion {
         }
 
 
-        public bool Save( [NotNull] Map mapToSave, [NotNull] string fileName ) {
-            if( mapToSave == null ) throw new ArgumentNullException( "mapToSave" );
-            if( fileName == null ) throw new ArgumentNullException( "fileName" );
+        public bool Save([NotNull] Map mapToSave, [NotNull] string fileName)
+        {
+            if (mapToSave == null) throw new ArgumentNullException("mapToSave");
+            if (fileName == null) throw new ArgumentNullException("fileName");
             throw new NotImplementedException();
         }
 
 
-        static bool MemCmp( [NotNull] IList<byte> data, int offset, [NotNull] string value ) {
-            if( data == null ) throw new ArgumentNullException( "data" );
-            if( value == null ) throw new ArgumentNullException( "value" );
+        static bool MemCmp([NotNull] IList<byte> data, int offset, [NotNull] string value)
+        {
+            if (data == null) throw new ArgumentNullException("data");
+            if (value == null) throw new ArgumentNullException("value");
             // ReSharper disable LoopCanBeConvertedToQuery
-            for( int i = 0; i < value.Length; i++ ) {
-                if( offset + i >= data.Count || data[offset + i] != value[i] ) return false;
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (offset + i >= data.Count || data[offset + i] != value[i]) return false;
             }
             // ReSharper restore LoopCanBeConvertedToQuery
             return true;
