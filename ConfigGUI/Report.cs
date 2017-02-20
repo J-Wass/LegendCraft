d9539490d97ebdee53ebf638839d64e8c8221e05
UnknownShadow200 <unknownshadow200@gmail.com>
2017-02-20 16:22:16 +1100

    Remove references to unused assemblies

--- ConfigGUI/Report.cs
+++ ConfigGUI/Report.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Management;
 using System.Net;
 using System.Net.Cache;
-using System.Reflection;
-using System.Management;
+using System.Text;
+using System.Windows.Forms;
 
 namespace fCraft.ConfigGUI
 {
